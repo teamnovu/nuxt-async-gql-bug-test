0f12141591bd2470f537436169fc6dad440f6722
Tom Diggelmann <tom.diggelmann@novu.ch>
2023-03-08 13:34:09 +0100

    add graphql example code

--- app.vue
+++ app.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <pre>{{ data }}</pre>
 </template>
+
+<script setup>
+const config = useRuntimeConfig()
+
+// this works
+// const data = await useAsyncData(() => $fetch('https://jsonplaceholder.typicode.com/todos/1'))
+
+// this does not work
+// const { data } = await useAsyncData(() => GqlPost())
+
+// this works
+// const { data } = await useAsyncData(() => $fetch(config.GQL_HOST, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     query: `
+// query Post {
+//   post(id: 1) {
+//     id
+//     title
+//     body
+//   }
+// }`,
+//   }),
+// }))
+
+// this does not work
+useGqlHeaders({ Accept: 'application/json' })
+const { data } = await useAsyncGql({
+  operation: 'Post',
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    runtimeConfig: {
+        public: {
+            GQL_HOST: 'https://graphqlzero.almansi.me/api',
+        },
+    },
 
+    modules: ['nuxt-graphql-client'],
 })
